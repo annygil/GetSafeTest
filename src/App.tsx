c95ac7b4e67334185775ab0af5bf3638ea7905ac
Anny Gil <annyvanessag@gmail.com>
2021-03-13 23:35:08 +0100

    Update App.tsx
    Add route not found

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,12 @@ const App = () => {
             </Route>
             <Route exact path="/purchased=dev_ins">
               <p> Developer Insurance Done!</p>
+              <Link to="/">Home</Link>
+            </Route>
+            <Route exact >
+              <h1>OOH SNAP!</h1>
+              <p> Not Found!</p>
+              <Link to="/">Home</Link>
             </Route>
           </Switch>
         </div>
