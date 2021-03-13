21e475773b4d7cd0272ca32b0994762730bab11a
Anny Gil <annyvanessag@gmail.com>
2021-03-13 20:56:13 +0100

    Update EmailStep.tsx
    Refactorin EmailStep structure

--- src/buyflow/EmailStep.tsx
+++ src/buyflow/EmailStep.tsx
@@ -1,15 +1,39 @@
-  import React, { useState } from 'react';
-
+import React, { useState,useEffect } from 'react';
+import useForm  from '../Hooks/Validations/useForm';
+import validate from '../Hooks/Validations/validateInputs';
+import { FormInput } from './../components/FormInput';
+import { ButtonNext } from './../components/ButtonNext';
 interface EmailStepProps {
     cb: (field: string, value: string) => void,
 }
 
-const EmailStep: React.FC<EmailStepProps> = (props) => {
-    const [email, setEmail] = useState('');
+
+const EmailStep: React.FC<EmailStepProps> = ( props ) => {
+    const { handleError, values, errors } = useForm( validate );
+
+    const handleNextStep = () => {
+        if( values.email.length > 0 && errors.emailError?.length === 0){
+            props.cb('email', values.email)
+        }
+   };
+
     return <>
-        <div>Email: <input type='email' onChange={({target: {value}}) => {setEmail(value)}} value={email}></input></div>
-        <button onClick={() => props.cb('email', email)}>Next</button>
+        <FormInput 
+            label='Email: '
+            id='email'
+            type='email'
+            name= 'email'
+            placeholder='Enter your email'
+            onChange={handleError}
+            errorMessage={errors.emailError}
+            value={values.email}/>
+        <ButtonNext
+            onClick={handleNextStep} 
+            disabled={values.email.length>0 && errors.emailError?.length==0 ? false : true}
+            name='Next'
+        />
     </>;
 };
+
 
 export default EmailStep;
